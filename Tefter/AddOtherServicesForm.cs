e3504938224410837c2c6605ea4313cab209822d
mitko <mitko@DESKTOP-9S8NSTL>
2022-04-15 17:09:27 +0300

    Modified AddOtherServicesForm

--- Tefter/AddOtherServicesForm.cs
+++ Tefter/AddOtherServicesForm.cs
@@ -27,8 +27,8 @@
             this.dbContext = dbContext;
             this.logger = logger;
 
-            Car = car;
-            OtherServicesDataGridView = otherServicesDataGridView;
+            this.Car = car;
+            this.OtherServicesDataGridView = otherServicesDataGridView;
         }
 
         public Car Car { get; set; }
@@ -37,59 +37,59 @@
 
         private void AddOtherServicesRow_Click(object sender, EventArgs e)
         {
+            var sb = new StringBuilder();
+            var emptyOrWrongFields = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(CurrentKilometers_TextBox.Text.Trim()))
+            {
+                emptyOrWrongFields.Add("Км");
+            }
+
+            var currentKilometersRegex = new Regex("^([0-9]*)$|^([0-9]* [0-9]*)$");
+
+            if (!currentKilometersRegex.IsMatch(CurrentKilometers_TextBox.Text.Trim()))
+            {
+                emptyOrWrongFields.Add("Км");
+            }
+
+            if (emptyOrWrongFields.Count() > 0)
+            {
+                sb.AppendLine("Моля въведете коректни данни за следните полета: ");
+                sb.AppendLine(string.Join(", ", emptyOrWrongFields.Select(x => x)));
+                MessageBox.Show(sb.ToString());
+                return;
+            }
+
+            var dateMadeChanges = DateMadeChanges_DatePicker.Value;
+            var kilometers = CurrentKilometers_TextBox.Text.Trim();
+            var serviceMade = Description_TextBox.Text.Trim();
+
+            var toStringedDate = dateMadeChanges.ToString("dd.M.yyyy HH:mm:ss");
+            var jsonData = new OtherServicesJsonData(toStringedDate, kilometers, serviceMade);
+            var data = JsonConvert.SerializeObject(jsonData);
+            var otherServices = new OtherService(data);
+
+            this.Car.OtherServices.Add(otherServices);
+
             try
             {
-                var dateMadeChanges = DateMadeChanges_DatePicker.Value;
-                var kilometers = CurrentKilometers_TextBox.Text.Trim();
-                var serviceMade = Description_TextBox.Text.Trim();
-
-                var sb = new StringBuilder();
-                var emptyOrWrongFields = new List<string>();
-
-                if (string.IsNullOrWhiteSpace(kilometers))
-                {
-                    emptyOrWrongFields.Add("Км");
-                }
-
-                var currentKilometersRegex = new Regex("^([0-9]*)$|^([0-9]* [0-9]*)$");
-
-                if (!currentKilometersRegex.IsMatch(kilometers))
-                {
-                    emptyOrWrongFields.Add("Км");
-                }
-
-                if (emptyOrWrongFields.Count() > 0)
-                {
-                    sb.AppendLine("Моля въведете коректни данни за следните полета: ");
-                    sb.AppendLine(string.Join(", ", emptyOrWrongFields.Select(x => x)));
-                    MessageBox.Show(sb.ToString());
-                    return;
-                }
-
-                var toStringedDate = dateMadeChanges.ToString("dd.M.yyyy HH:mm:ss");
-                var jsonData = new OtherServicesJsonData(toStringedDate, kilometers, serviceMade);
-                var data = JsonConvert.SerializeObject(jsonData);
-                var otherServices = new OtherService(data);
-
-                Car.OtherServices.Add(otherServices);
-
-                dbContext.SaveChanges();
-
-                OtherServicesDataGridView.Rows.Add();
-                var rowsCountWithNewRow = OtherServicesDataGridView.Rows.Count - 1;
-
-                OtherServicesDataGridView.Rows[rowsCountWithNewRow].Cells[0].Value = Car.OtherServices.First().Id;
-                OtherServicesDataGridView.Rows[rowsCountWithNewRow].Cells[1].Value = jsonData.DateMadeChanges;
-                OtherServicesDataGridView.Rows[rowsCountWithNewRow].Cells[2].Value = jsonData.Kilometers;
-                OtherServicesDataGridView.Rows[rowsCountWithNewRow].Cells[3].Value = jsonData.ServiceMade;
-
-                Close();
+                this.dbContext.SaveChanges();
             }
             catch (Exception ex)
             {
                 logger.WriteLine($"AddOtherServicesForm.AddOtherServicesRow_Click: {ex}");
                 MessageBox.Show("Възникна неочаквана грешка!");
             }
+
+            this.OtherServicesDataGridView.Rows.Add();
+            var rowsCountWithNewRow = OtherServicesDataGridView.Rows.Count - 1;
+
+            this.OtherServicesDataGridView.Rows[rowsCountWithNewRow].Cells[0].Value = Car.OtherServices.First().Id;
+            this.OtherServicesDataGridView.Rows[rowsCountWithNewRow].Cells[1].Value = jsonData.DateMadeChanges;
+            this.OtherServicesDataGridView.Rows[rowsCountWithNewRow].Cells[2].Value = jsonData.Kilometers;
+            this.OtherServicesDataGridView.Rows[rowsCountWithNewRow].Cells[3].Value = jsonData.ServiceMade;
+
+            this.Close();
         }
     }
 }
