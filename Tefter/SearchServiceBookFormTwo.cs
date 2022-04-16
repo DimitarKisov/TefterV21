e3e049640bf113bf919c6f71a25d3228313e390e
mitko <mitko@DESKTOP-9S8NSTL>
2022-04-16 13:11:29 +0300

    Modified SearchSeviceBookFormTwo

--- Tefter/SearchServiceBookFormTwo.cs
+++ Tefter/SearchServiceBookFormTwo.cs
@@ -11,7 +11,6 @@
     using System.Collections.Generic;
     using System.Data;
     using System.Text.RegularExpressions;
-    using System.Drawing;
 
     public partial class SearchServiceBookFormTwo : Form
     {
@@ -25,7 +24,7 @@
             this.dbContext = dbContext;
             this.logger = logger;
 
-            Car = car;
+            this.Car = car;
         }
 
         public Car Car { get; set; }
@@ -34,199 +33,178 @@
         {
             try
             {
-                Car.OilAndFilters = dbContext.OilAndFilters.Where(x => x.CarId == Car.Id).ToList();
-
-                OilAndFiltersDataGridView.Columns["Id"].Visible = false;
-
-                for (int i = 0; i < Car.OilAndFilters.Count(); i++)
-                {
-                    if (Car.OilAndFilters.Count() - 1 >= i)
-                    {
-                        OilAndFiltersDataGridView.Rows.Add();
-                    }
-
-                    var parsedJson = Car.OilAndFilters[i].ParseData<OilAndFiltersJsonData>();
-                    OilAndFiltersDataGridView.Rows[i].Cells[0].Value = Car.OilAndFilters[i].Id;
-                    OilAndFiltersDataGridView.Rows[i].Cells[1].Value = parsedJson.Date;
-                    OilAndFiltersDataGridView.Rows[i].Cells[2].Value = parsedJson.Kilometers;
-                    OilAndFiltersDataGridView.Rows[i].Cells[3].Value = parsedJson.Oil;
-                    OilAndFiltersDataGridView.Rows[i].Cells[4].Value = parsedJson.NextChange;
-                    OilAndFiltersDataGridView.Rows[i].Cells[5].Value = parsedJson.OilFilter;
-                    OilAndFiltersDataGridView.Rows[i].Cells[6].Value = parsedJson.FuelFilter;
-                    OilAndFiltersDataGridView.Rows[i].Cells[7].Value = parsedJson.AirFilter;
-                    OilAndFiltersDataGridView.Rows[i].Cells[8].Value = parsedJson.CoupeFilter;
-                }
+                this.Car.OilAndFilters = this.dbContext
+                    .OilAndFilters
+                    .Where(x => x.CarId == Car.Id)
+                    .ToList();
             }
             catch (Exception ex)
             {
-                logger.WriteLine($"SearchServiceBookFormTwo.SearchServiceBookFormTwo_Load: {ex}");
+                this.logger.WriteLine($"SearchServiceBookFormTwo.SearchServiceBookFormTwo_Load: {ex}");
                 MessageBox.Show("Възникна неочаквана грешка!");
+            }
+
+            OilAndFiltersDataGridView.Columns["Id"].Visible = false;
+
+            for (int i = 0; i < Car.OilAndFilters.Count(); i++)
+            {
+                if (Car.OilAndFilters.Count() - 1 >= i)
+                {
+                    OilAndFiltersDataGridView.Rows.Add();
+                }
+
+                var parsedJson = Car.OilAndFilters[i].ParseData<OilAndFiltersJsonData>();
+                OilAndFiltersDataGridView.Rows[i].Cells[0].Value = Car.OilAndFilters[i].Id;
+                OilAndFiltersDataGridView.Rows[i].Cells[1].Value = parsedJson.Date;
+                OilAndFiltersDataGridView.Rows[i].Cells[2].Value = parsedJson.Kilometers;
+                OilAndFiltersDataGridView.Rows[i].Cells[3].Value = parsedJson.Oil;
+                OilAndFiltersDataGridView.Rows[i].Cells[4].Value = parsedJson.NextChange;
+                OilAndFiltersDataGridView.Rows[i].Cells[5].Value = parsedJson.OilFilter;
+                OilAndFiltersDataGridView.Rows[i].Cells[6].Value = parsedJson.FuelFilter;
+                OilAndFiltersDataGridView.Rows[i].Cells[7].Value = parsedJson.AirFilter;
+                OilAndFiltersDataGridView.Rows[i].Cells[8].Value = parsedJson.CoupeFilter;
             }
         }
 
         private void Search_BackButton_Click(object sender, EventArgs e)
         {
-            try
-            {
-                var searchServiceBookFirstPage = new SearchServiceBookFormOne(Car, dbContext, logger);
-                Close();
-                searchServiceBookFirstPage.Show();
-            }
-            catch (Exception ex)
-            {
-                logger.WriteLine($"SearchServiceBookFormTwo.Search_BackButton_Click: {ex}");
-                MessageBox.Show("Възникна неочаквана грешка!");
-            }
+            var searchServiceBookFirstPage = new SearchServiceBookFormOne(Car, dbContext, logger);
+            this.Close();
+            searchServiceBookFirstPage.Show();
         }
 
         private void AddButton_Click(object sender, EventArgs e)
         {
-            try
-            {
-                var addOilAndFiltersForm = new AddOilAndFiltersForm(Car, OilAndFiltersDataGridView, dbContext, logger);
-                addOilAndFiltersForm.Show();
-            }
-            catch (Exception ex)
-            {
-                logger.WriteLine($"SearchServiceBookFormTwo.AddButton_Click: {ex}");
-                MessageBox.Show("Възникна неочаквана грешка!");
-            }
+            var addOilAndFiltersForm = new AddOilAndFiltersForm(Car, OilAndFiltersDataGridView, dbContext, logger);
+            addOilAndFiltersForm.Show();
         }
 
         private void SaveChangesButton_Click(object sender, EventArgs e)
         {
-            try
+            var sb = new StringBuilder();
+            var emptyOrWrongFields = new List<string>();
+
+            for (int i = 0; i < Car.OilAndFilters.Count(); i++)
             {
-                var sb = new StringBuilder();
-                var emptyOrWrongFields = new List<string>();
+                var id = (int)OilAndFiltersDataGridView.Rows[i].Cells[0].Value;
+                var date = ((string)OilAndFiltersDataGridView.Rows[i].Cells[1].Value);
+                var kilometers = (string)OilAndFiltersDataGridView.Rows[i].Cells[2].Value;
+                var oil = (string)OilAndFiltersDataGridView.Rows[i].Cells[3].Value;
+                var nextChange = (string)OilAndFiltersDataGridView.Rows[i].Cells[4].Value;
+                var oilFilter = (string)OilAndFiltersDataGridView.Rows[i].Cells[5].Value;
+                var fuelFilter = (string)OilAndFiltersDataGridView.Rows[i].Cells[6].Value;
+                var airFilter = (string)OilAndFiltersDataGridView.Rows[i].Cells[7].Value;
+                var coupeFilter = (string)OilAndFiltersDataGridView.Rows[i].Cells[8].Value;
 
-                for (int i = 0; i < Car.OilAndFilters.Count(); i++)
+                if (string.IsNullOrWhiteSpace(date))
                 {
-                    var id = (int)OilAndFiltersDataGridView.Rows[i].Cells[0].Value;
-                    var date = ((string)OilAndFiltersDataGridView.Rows[i].Cells[1].Value);
-                    var kilometers = (string)OilAndFiltersDataGridView.Rows[i].Cells[2].Value;
-                    var oil = (string)OilAndFiltersDataGridView.Rows[i].Cells[3].Value;
-                    var nextChange = (string)OilAndFiltersDataGridView.Rows[i].Cells[4].Value;
-                    var oilFilter = (string)OilAndFiltersDataGridView.Rows[i].Cells[5].Value;
-                    var fuelFilter = (string)OilAndFiltersDataGridView.Rows[i].Cells[6].Value;
-                    var airFilter = (string)OilAndFiltersDataGridView.Rows[i].Cells[7].Value;
-                    var coupeFilter = (string)OilAndFiltersDataGridView.Rows[i].Cells[8].Value;
-
-                    if (string.IsNullOrWhiteSpace(date))
-                    {
-                        emptyOrWrongFields.Add("Дата");
-                    }
-
-                    if (string.IsNullOrWhiteSpace(oil))
-                    {
-                        emptyOrWrongFields.Add("Масло");
-                    }
-
-                    if (string.IsNullOrWhiteSpace(kilometers))
-                    {
-                        emptyOrWrongFields.Add("Км");
-                    }
-
-                    if (string.IsNullOrWhiteSpace(nextChange))
-                    {
-                        emptyOrWrongFields.Add("Следваща смяна на (км)");
-                    }
-
-                    if (emptyOrWrongFields.Count() > 0)
-                    {
-                        sb.AppendLine("Моля попълнете следните полета: ");
-                        sb.AppendLine(string.Join(", ", emptyOrWrongFields.Select(x => x)));
-
-                        MessageBox.Show(sb.ToString());
-                        return;
-                    }
-
-                    var dateTimeMadeChanges = new Regex(@"^([1-9]|([012][0-9])|(3[01]))\.([0]{0,1}[1-9]|1[012])\.\d\d\d\d (20|21|22|23|[0-1]?\d):[0-5]?\d:[0-5]?\d$");
-                    var currentKilometersRegex = new Regex("^([0-9]*)$|^([0-9]* [0-9]*)$");
-                    var nextOilChangeKilometersRegex = new Regex("^([0-9]*)$|^([0-9]* [0-9]*)$");
-
-                    if (!dateTimeMadeChanges.IsMatch(date))
-                    {
-                        emptyOrWrongFields.Add("Дата");
-                    }
-
-                    if (!currentKilometersRegex.IsMatch(kilometers))
-                    {
-                        emptyOrWrongFields.Add("Км");
-                    }
-
-                    if (!nextOilChangeKilometersRegex.IsMatch(nextChange))
-                    {
-                        emptyOrWrongFields.Add("Следваща смяна на (км)");
-                    }
-
-                    if (emptyOrWrongFields.Count() > 0)
-                    {
-                        sb.AppendLine("Моля въведете коректни данни за следните полета: ");
-                        sb.AppendLine(string.Join(", ", emptyOrWrongFields.Select(x => x)));
-
-                        MessageBox.Show(sb.ToString());
-                        return;
-                    }
-
-                    var oilAndFilters = new OilAndFiltersJsonData(date, kilometers, oil, nextChange, oilFilter, fuelFilter, airFilter, coupeFilter);
-                    var parsedJson = JsonConvert.SerializeObject(oilAndFilters);
-
-                    Car.OilAndFilters.FirstOrDefault(x => x.Id == id).Data = parsedJson;
+                    emptyOrWrongFields.Add("Дата");
                 }
 
-                dbContext.SaveChanges();
+                if (string.IsNullOrWhiteSpace(oil))
+                {
+                    emptyOrWrongFields.Add("Масло");
+                }
 
-                MessageBox.Show("Успешно направени промени.");
+                if (string.IsNullOrWhiteSpace(kilometers))
+                {
+                    emptyOrWrongFields.Add("Км");
+                }
+
+                if (string.IsNullOrWhiteSpace(nextChange))
+                {
+                    emptyOrWrongFields.Add("Следваща смяна на (км)");
+                }
+
+                if (emptyOrWrongFields.Count() > 0)
+                {
+                    sb.AppendLine("Моля попълнете следните полета: ");
+                    sb.AppendLine(string.Join(", ", emptyOrWrongFields.Select(x => x)));
+
+                    MessageBox.Show(sb.ToString());
+                    return;
+                }
+
+                var dateTimeMadeChanges = new Regex(@"^([1-9]|([012][0-9])|(3[01]))\.([0]{0,1}[1-9]|1[012])\.\d\d\d\d (20|21|22|23|[0-1]?\d):[0-5]?\d:[0-5]?\d$");
+                var currentKilometersRegex = new Regex("^([0-9]*)$|^([0-9]* [0-9]*)$");
+                var nextOilChangeKilometersRegex = new Regex("^([0-9]*)$|^([0-9]* [0-9]*)$");
+
+                if (!dateTimeMadeChanges.IsMatch(date))
+                {
+                    emptyOrWrongFields.Add("Дата");
+                }
+
+                if (!currentKilometersRegex.IsMatch(kilometers))
+                {
+                    emptyOrWrongFields.Add("Км");
+                }
+
+                if (!nextOilChangeKilometersRegex.IsMatch(nextChange))
+                {
+                    emptyOrWrongFields.Add("Следваща смяна на (км)");
+                }
+
+                if (emptyOrWrongFields.Count() > 0)
+                {
+                    sb.AppendLine("Моля въведете коректни данни за следните полета: ");
+                    sb.AppendLine(string.Join(", ", emptyOrWrongFields.Select(x => x)));
+
+                    MessageBox.Show(sb.ToString());
+                    return;
+                }
+
+                var oilAndFilters = new OilAndFiltersJsonData(date, kilometers, oil, nextChange, oilFilter, fuelFilter, airFilter, coupeFilter);
+                var parsedJson = JsonConvert.SerializeObject(oilAndFilters);
+
+                this.Car.OilAndFilters.FirstOrDefault(x => x.Id == id).Data = parsedJson;
+            }
+
+            try
+            {
+                this.dbContext.SaveChanges();
             }
             catch (Exception ex)
             {
-                logger.WriteLine($"SearchServiceBookFormTwo.SaveChangesButton_Click: {ex}");
+                this.logger.WriteLine($"SearchServiceBookFormTwo.SaveChangesButton_Click: {ex}");
                 MessageBox.Show("Възникна неочаквана грешка!");
             }
+
+            MessageBox.Show("Успешно направени промени.");
         }
 
         private void DeleteRecordButton_Click(object sender, EventArgs e)
         {
+            var selectedRows = OilAndFiltersDataGridView.SelectedRows;
+
+            foreach (DataGridViewRow row in selectedRows)
+            {
+                var id = (int)row.Cells["Id"].Value;
+                var itemForRemove = this.Car.OilAndFilters.FirstOrDefault(x => x.Id == id);
+
+                this.Car.OilAndFilters.Remove(itemForRemove);
+                OilAndFiltersDataGridView.Rows.Remove(row);
+            }
+
             try
             {
-                var selectedRows = OilAndFiltersDataGridView.SelectedRows;
-
-                foreach (DataGridViewRow row in selectedRows)
-                {
-                    var id = (int)row.Cells["Id"].Value;
-                    var itemForRemove = Car.OilAndFilters.FirstOrDefault(x => x.Id == id);
-
-                    Car.OilAndFilters.Remove(itemForRemove);
-                    OilAndFiltersDataGridView.Rows.Remove(row);
-                }
-
-                dbContext.SaveChanges();
-
-                MessageBox.Show("Успешно изтрит запис.");
+                this.dbContext.SaveChanges();
             }
             catch (Exception ex)
             {
-                logger.WriteLine($"SearchServiceBookFormTwo.DeleteRecordButton_Click: {ex}");
+                this.logger.WriteLine($"SearchServiceBookFormTwo.DeleteRecordButton_Click: {ex}");
                 MessageBox.Show("Възникна неочаквана грешка!");
             }
+
+            MessageBox.Show("Успешно изтрит запис.");
         }
 
         private void SearchServiceBookFormTwo_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            try
+            if (e.ColumnIndex >= 0 && e.RowIndex >= 0)
             {
-                if (e.ColumnIndex >= 0 && e.RowIndex >= 0)
-                {
-                    var cell = OilAndFiltersDataGridView[e.ColumnIndex, e.RowIndex];
-                    OilAndFiltersDataGridView.CurrentCell = cell;
-                    OilAndFiltersDataGridView.BeginEdit(true);
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.WriteLine($"SearchServiceBookFormTwo.SearchServiceBookFormTwo_CellDoubleClick: {ex}");
-                MessageBox.Show("Възникна неочаквана грешка!");
+                var cell = OilAndFiltersDataGridView[e.ColumnIndex, e.RowIndex];
+                OilAndFiltersDataGridView.CurrentCell = cell;
+                OilAndFiltersDataGridView.BeginEdit(true);
             }
         }
     }
